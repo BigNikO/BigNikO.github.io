550796be4fbb2a02427b194e5c297951c89b9811
BigNikO <big_nik0@yahoo.com>
2019-09-29 12:32:19 +0300

    First steps

---
+++ src/Components/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const  HomePage = () =>{
+    return (
+        <div className="App">
+            <header>
+              <h1>My React page</h1>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+
+export default HomePage;
