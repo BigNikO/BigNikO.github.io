5d5280577cef0979d13e7f2fcf1b7a40ae5482e6
BigNikO <big_nik0@yahoo.com>
2019-09-29 12:10:40 +0300

    Updates

---
+++ precache-manifest.493bed51e2d4a284cf0586cec3915ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d951716f19452971900ee6d8de80fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05a83d6cb8013531f4c2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "64fcd4894fc9e662b04c",
+    "url": "/static/js/2.a2e4917e.chunk.js"
+  },
+  {
+    "revision": "05a83d6cb8013531f4c2",
+    "url": "/static/js/main.7bf6272a.chunk.js"
+  },
+  {
+    "revision": "6344a1373ddf63015840",
+    "url": "/static/js/runtime-main.05bd8878.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
