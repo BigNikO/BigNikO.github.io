7a997f12678920c2a4da4e688905a5d344a3ec73
BigNikO <big_nik0@yahoo.com>
2019-09-29 12:38:19 +0300

    Clean up

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -5,10 +5,10 @@ const  HomePage = () =>{
     return (
         <div className="App">
             <header>
-              <h1>My React page</h1>
+                <h1>My React page</h1>
             </header>
             <main>
-
+                <p>Hello!</p>
             </main>
         </div>
     );
