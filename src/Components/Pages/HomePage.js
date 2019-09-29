6bacff7811d420df3749bf28b8837bf110a73430
BigNikO <big_nik0@yahoo.com>
2019-09-29 14:08:46 +0300

    Fix security alerts

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-
-const  HomePage = () =>{
-    return (
-        <div className="App">
-            <header>
-                <h1>My React page</h1>
-            </header>
-            <main>
-                <p>Hello!</p>
-            </main>
-        </div>
-    );
-}
+const HomePage = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>My React page</h1>
+      </header>
+      <main>
+        <p>Hello!</p>
+      </main>
+    </div>
+  );
+};
 
 export default HomePage;
